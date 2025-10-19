065012a616ea4e2d38c25a149bf61facd40b583c
soulframespvr-stack <soulframespvr@gmail.com>
2025-10-20 00:29:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // 👈 VERY IMPORTANT for GitHub Pages or Netlify
   server: {
     host: "::",
     port: 8080,
